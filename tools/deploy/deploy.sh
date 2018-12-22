880013f10bbc549203ba2e96198ebe7604a0380b
Stanislas Nanchen <stan@hood.ch>
2018-12-22 13:19:54 +0100

    refactor the artifacts and core stack.

--- tools/deploy/deploy.sh
+++ tools/deploy/deploy.sh
@@ -4,12 +4,12 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 VERSION=$(git describe --match "v*" --dirty=--DIRTY-- | sed 's:^.\(.*\)$:\1:')
-ARTEFACTS_STACK_NAME=HyperdriveArtefacts
+CORE_STACK_NAME=HyperdriveArtefacts
 
 function init () {
     aws cloudformation deploy \
-        --stack-name=${ARTEFACTS_STACK_NAME} \
-        --template-file=${SCRIPT_DIR}/hyperdrive_artefacts.yaml \
+        --stack-name=${CORE_STACK_NAME} \
+        --template-file=${SCRIPT_DIR}/hyperdrive_core.yaml \
         --parameter-override \
             BaseDomainName=$1 \
             Version=${VERSION}
@@ -19,11 +19,11 @@ function package () {
     cd ${SCRIPT_DIR}/../..
     goreleaser --snapshot --rm-dist
     cd ${SCRIPT_DIR}
-    S3_BUCKET=$(aws cloudformation describe-stacks --stack-name ${ARTEFACTS_STACK_NAME} | jq -r '.Stacks[0].Outputs | map(select(.OutputKey=="ArtefactsBucketName"))[0].OutputValue')
+    S3_BUCKET=$(aws cloudformation describe-stacks --stack-name ${CORE_STACK_NAME} | jq -r '.Stacks[0].Outputs | map(select(.OutputKey=="ArtifactsBucketName"))[0].OutputValue')
     aws cloudformation package \
-        --template-file=hyperdrive_core.yaml \
+        --template-file=hyperdrive_lambda.yaml \
         --s3-bucket=${S3_BUCKET} \
-        --s3-prefix=hyperdrive \
+        --s3-prefix=.hyperdrive/${VERSION} \
         --output-template=${SCRIPT_DIR}/../../dist/${VERSION}.yaml
 }
 
@@ -33,17 +33,17 @@ function deploy () {
     aws cloudformation deploy \
         --capabilities CAPABILITY_NAMED_IAM \
         --template-file ${VERSION}.yaml \
-        --stack-name HyperdriveCore \
+        --stack-name HyperdriveLambda \
         --parameter-override \
             Version=${VERSION} \
             HyperdriveKmsKeyId=${key_id}
 }
 
 case $1 in
-    "init") init ${2};;
-	"package") package;;
-	"deploy") deploy;;
-	"package-deploy")
+    "core") init ${2};;
+	"lambda-package") package;;
+	"lambda-deploy") deploy;;
+	"lambda-package-deploy")
 	    package
 	    deploy
 	    ;;
